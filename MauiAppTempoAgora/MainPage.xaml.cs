815603ddd96587da93f40945928076bea888b34a
silva <silva@NOTEDOGUIS>
2026-09-20 12:25:49 -0300

    Melhorando a Messegem de erro na conexão

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -14,43 +14,43 @@ namespace MauiAppTempoAgora
         {
             try
             {
-                    if (!string.IsNullOrEmpty(txt_cidade.Text))
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                    if (t != null)
                     {
-                        Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                        string dados_previsao = "";
 
-                        if (t != null)
-                        {
-                            string dados_previsao = "";
+                        dados_previsao =
 
-                            dados_previsao =
+                                         $"Descrição: {t.description} \n" +
+                                         $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Vel Vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n";
 
-                                             $"Descrição: {t.description} \n" +
-                                             $"Latitude: {t.lat} \n" +
-                                             $"Longitude: {t.lon} \n" +
-                                             $"Nascer do Sol: {t.sunrise} \n" +
-                                             $"Por do Sol: {t.sunset} \n" +
-                                             $"Temp Máx: {t.temp_max} \n" +
-                                             $"Temp Min: {t.temp_min} \n" +
-                                             $"Vel Vento: {t.speed} \n" +
-                                             $"Visibilidade: {t.visibility} \n";
+                        lbl_res.Text = dados_previsao;
 
-                            lbl_res.Text = dados_previsao;
-
-                        }
-                        else
-                        {
-                            await DisplayAlertAsync("Ops", "A cidade: " + txt_cidade.Text + "\nNão foi encontrada", "OK");
-                            lbl_res.Text = "Sem dados de Previsão";
-                        }
                     }
                     else
                     {
-                        lbl_res.Text = "Preencha a cidade.";
+                        await DisplayAlertAsync("Ops", "A cidade: " + txt_cidade.Text + "\nNão foi encontrada", "OK");
+                        lbl_res.Text = "Sem dados de Previsão";
                     }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
             }
             catch (Exception ex)
             {
-                await DisplayAlertAsync("Ops", ex.Message, "OK");
+                await DisplayAlertAsync("Ops", "Verifique sua conexão com a Internet\n" + ex.Message, "OK");
             }
         }
     }
