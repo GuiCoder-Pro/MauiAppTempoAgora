6df15f1ae96ee03114b39372a7aee7bf28a80792
silva <silva@NOTEDOGUIS>
2026-09-19 23:03:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if () {
+                    if (!string.IsNullOrEmpty(txt_cidade.Text))
+                    {
+                        Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+
+                        if (t != null)
+                        {
+                            string dados_previsao = "";
+
+                            dados_previsao =
+
+                                             $"Descrição: {t.description} \n" +
+                                             $"Latitude: {t.lat} \n" +
+                                             $"Longitude: {t.lon} \n" +
+                                             $"Nascer do Sol: {t.sunrise} \n" +
+                                             $"Por do Sol: {t.sunset} \n" +
+                                             $"Temp Máx: {t.temp_max} \n" +
+                                             $"Temp Min: {t.temp_min} \n" +
+                                             $"Vel Vento: {t.speed} \n" +
+                                             $"Visibilidade: {t.visibility} \n";
+
+                            lbl_res.Text = dados_previsao;
+
+                        }
+                        else
+                        {
+                            await DisplayAlertAsync("Ops", "A cidade: " + txt_cidade.Text + "\nNão foi encontrada", "OK");
+                            lbl_res.Text = "Sem dados de Previsão";
+                        }
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Preencha a cidade.";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
